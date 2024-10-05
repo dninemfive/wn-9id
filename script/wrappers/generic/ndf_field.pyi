d275f19d24465c435cf4644e9c59c7e28f12b5d0
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-10-04 23:18:05 -0600

    well i found out these are called descriptors or smth but i'mma shelve it for now

--- script/wrappers/generic/ndf_field.pyi
+++ script/wrappers/generic/ndf_field.pyi
@@ -1,14 +1,23 @@
-from typing import Generic, TypeVar
-from ndf_parse.model import ListRow, MapRow, MemberRow
+from typing import Generic, Self, TypeVar
+from ndf_parse.model import List, ListRow, Map, MapRow, MemberRow, Object
 
 T = TypeVar('T')
+NdfItemReference = List | Map | Object
 NdfFieldReference = ListRow | MapRow | MemberRow
 
 class NdfFieldWrapper(Generic[T]):
-    def set(field: NdfFieldReference, value: T) -> None:
+    def __init__(self: Self, ndf: NdfItemReference, name: str):
+        if isinstance(ndf, List):
+            self.ref: ListRow = ndf.by_name(name)
+        elif isinstance(ndf, Map):
+            self.ref: MapRow = ndf.by_key(name)
+        elif isinstance(ndf, Object):
+            self.ref: MemberRow = ndf.by_member(name)
+
+    def __set__(field: NdfFieldReference, value: T) -> None:
         pass
 
-    def get(field: NdfFieldReference) -> T:
+    def __get__(field: NdfFieldReference) -> T:
         pass
 
 class IntWrapper(NdfFieldWrapper[int]):
