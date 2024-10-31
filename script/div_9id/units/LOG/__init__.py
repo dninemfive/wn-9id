cc1c7c623c6663166362a1472a00878b3369aac5
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-10-30 20:08:24 -0600

    replace M1075 PLS with HEMTT

--- script/div_9id/units/LOG/__init__.py
+++ script/div_9id/units/LOG/__init__.py
@@ -5,7 +5,6 @@ from mw2.utils.types.message import Message
 
 from .cmd_m997_tc3v import create as cmd_m997_tc3v_
 from .m998_humvee_supply import create as m998_humvee_supply_
-from .m1075_pls import create as m1075_pls_
 
 
 def group(registry: DivisionUnitRegistry, parent_msg: Message | None = None) -> UnitGroup:
@@ -27,7 +26,7 @@ def group(registry: DivisionUnitRegistry, parent_msg: Message | None = None) -> 
                 u('FOB_US', 1),
                 u(m998_humvee_supply_, 2),
                 u('M35_supply_US', 1),
-                u(m1075_pls_, 1),
+                u('HEMTT_US', 2),
                 u('UH60A_Supply_US', 2),
                 u('CH47_Super_Chinook_US', 1),
             ]
